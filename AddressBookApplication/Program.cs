1afedb93005dbf9f397c5c81cb0aebec5438a918
Yatharth2609 <yatharth.mishra2002@gmail.com>
2025-03-12 23:56:04 +0530

    [Yatharth] Add: Inital DBContext Setup is done

---
+++ AddressBookApplication/Program.cs
@@ -0,0 +1,29 @@
+using System;
+using Microsoft.EntityFrameworkCore;
+using RepositoryLayer;
+using RepositoryLayer.Service;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+// Configure Entity Framework Core
+builder.Services.AddDbContext<AdressBookDBContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+app.UseAuthorization();
+app.MapControllers();
+app.Run();
